98fb78ba4d6d429d62ac6d4b0bbc04814f75a9fd
zuma-code <zumar53@protonmail.com>
2025-01-15 15:28:37 +0000

    until iteration 2

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 // Iteration 1: Names and Input
+ const hacker1 = "Ana";
+ let hackerName1 = `The driver's name is ${hacker1}`
+ console.log(hackerName1)
 
 
+ // Iteration 1: Names and Input
+ const hacker2 = "Gara";
+ let hackerName2 = `The driver's name is ${hacker2}`
+ console.log(hackerName2)
+
+ 
 // Iteration 2: Conditionals
+const hacker1 = "Ana"; // Driver's name
+const hacker2 = "Gara"; // Navigator's name
+
+if (hacker1.length > hacker2.length) {
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker1.length < hacker2.length) {
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else {
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+}
 
 
 // Iteration 3: Loops
