e889ace0f99077c93c1889eddc3c0ee302f1ce69
zuma-code <zumar53@protonmail.com>
2025-01-16 11:29:18 +0000

    lab done

--- index.js
+++ index.js
@@ -11,16 +11,80 @@
 
  
 // Iteration 2: Conditionals
-const hacker1 = "Ana"; // Driver's name
-const hacker2 = "Gara"; // Navigator's name
+const driver = "Ana"; // Driver's name
+const navigator = "Gara"; // Navigator's name
 
-if (hacker1.length > hacker2.length) {
-  console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
-} else if (hacker1.length < hacker2.length) {
-  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+if (driver.length > navigator.length) {
+  console.log(`The driver has the longest name, it has ${driver.length} characters.`);
+} else if (driver.length < navigator.length) {
+  console.log(`It seems that the navigator has the longest name, it has ${navigator.length} characters.`);
 } else {
-  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
+  console.log(`Wow, you both have equally long names, ${driver.length} characters!`);
 }
 
 
 // Iteration 3: Loops
+
+//Print the characters of the driver's name, separated by space, and in capital letters, i.e., "J O H N".
+const driver = "Ana"; 
+let spacedName = ""; 
+
+for (let i = 0; i < driver.length; i++) {
+
+  spacedName += driver[i].toUpperCase();
+  
+
+  if (i < driver.length - 1) {
+    spacedName += " ";
+  }
+}
+
+console.log(spacedName); 
+
+//Print all the characters of the navigator's name in reverse order, i.e., "nhoJ".
+const navigator = "Gara"; 
+let reversedName = "";
+
+for (let i = navigator.length - 1; i >= 0; i--) {
+    reversedName += navigator[i];
+}
+
+console.log(reversedName); 
+
+
+
+//Depending on the lexicographic order of the strings, print:
+
+const driver = "ana"; 
+const navigator = "gara"; 
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+let result = ""; 
+let minLength = Math.min(driver.length, navigator.length);
+
+
+for (let i = 0; i < minLength; i++) {
+  const driverIndex = alphabet.indexOf(driver[i]);
+  const navigatorIndex = alphabet.indexOf(navigator[i]);
+
+  if (driverIndex < navigatorIndex) {
+    result = "The driver's name goes first.";
+    break;
+  } else if (driverIndex > navigatorIndex) {
+    result = "Yo, the navigator goes first, definitely.";
+    break;
+  }
+}
+
+if (result === "") {
+  if (driver.length < navigator.length) {
+    result = "The driver's name goes first.";
+  } else if (driver.length > navigator.length) {
+    result = "Yo, the navigator goes first, definitely.";
+  } else {
+    result = "What?! You both have the same name?";
+  }
+}
+
+console.log(result);
